5c8abd73654e5c8c1d3e3e41b24180deefdcd214
Ray <ray.rodz.88@gmail.com>
2021-03-06 09:30:56 -0600

    videoList

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import SearchBar from './SearchBar'
 import youtube from '../apis/YouTube'
+import VideoList from './VideoList'
 //Class based componed because its holding state for entire application 
 
 class App extends React.Component{
-  onTermSubmit = (term) => {
-    youtube.get('/search', {
+  state = { 
+    videos: [],
+    selectedVideo: null
+  };
+
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
       params:{
         q: term
       }
     });
+    this.setState({ videos: response.data.items})
   };
+
+  onVideoSelect = (video) => {
+    console.log('from the app! ' , video);
+  };
+
   render(){
     return (
       <div className="ui container"> 
         <SearchBar onTermSubmit={this.onTermSubmit} /> 
+        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
       </div>
     );
   }
